bc0f23e2b789975848cc93605be8cd0fc0ac7605
vavarrm <vavarrm@gmail.com>
2018-02-10 20:49:46 +0700

    t

--- js/main.js
+++ js/main.js
@@ -1,105 +1,389 @@
-function userInformation() {
-
-	$.ajax({
-
-		url: "/api/Api/getUser?sess=" + sessionStorage.session,
-
-		type: "GET",
-
-		success: function (data) {
-
-			if (data.status) {
-
-				switch (data.status) {
-
-					case 100:
-
-						$('.jq-login-before').hide();
-
-						$('.jq-login-after').show();
-
-						$('.jq-member-nam').text(data.body.user.u_name);
-						$('.nam span').eq(0).html(data.body.user.balance);
-						$('.nam span').eq(1).html(data.body.user.no_read_message_total);
-
-					break;
-					case '999':
-						$('.jq-login-before').show();
-
-						$('.jq-login-after').hide();
-					break;
-
-				}
-
-			}
-
-		}
-
-	});
-
-}
-
-$(function() {
-  $.ajax({
-
-	url: "/api/Api/getFooterInfo",
-
-	type: "GET",
-
-	success: function (data) {
-
-
-		var _each_wechat = $('.jq-service-wechat');
-
-		var _each_QQ = $('.jq-service-QQ')
-
-		if (data.status) {
-
-			switch (data.status) {
-
-				case 100:
-
-					_each_wechat.append(
-
-						'<a href="javascript:void(0)" class="btn btn-bg-blue">微信客服</a>' +
-
-						'<div class="img-container">' +
-
-							'<img class="img absolute-center-xs" src="images/webconfig/' + data.body.list.wechat_qr_image.wc_value + '" alt="">' +
-
-						'</div>'
-
-					)
-
-					_each_QQ.append(
-
-						'<a href="javascript:void(0)" class="btn btn-bg-blue">QQ客服</a>' +
-
-						'<div class="img-container">' +
-
-							'<img class="img absolute-center-xs" src="images/webconfig/' + data.body.list.qq_qr_image.wc_value + '" alt="">' +
-
-						'</div>'
-
-					)
-
-			}
-
-		}
-
-	}
-
-});
-
-if (sessionStorage.session == undefined || sessionStorage.session == '') {
-
-	$('.jq-login-before').show();
-
-	$('.jq-login-after').hide();
-
-} else {
-
-	userInformation()
-
-}
+var _phone_key
+
+function setCookie(_sess) {
+
+	sessionStorage.session = _sess;
+
+};
+
+function clearCookie() {
+
+	sessionStorage.session = '';
+
+};
+
+function userInformation() {
+
+	$.ajax({
+
+		url: "/api/Api/getUser?sess=" + sessionStorage.session,
+
+		type: "GET",
+
+		success: function (data) {
+
+			if (data.status) {
+
+				switch (data.status) {
+
+					case 100:
+
+						$('.jq-login-before').hide();
+
+						$('.jq-login-after').show();
+
+						$('.jq-member-nam').text(data.body.user.u_name);
+						$('.nam span').eq(0).html(data.body.user.balance);
+						$('.nam span').eq(1).html(data.body.user.no_read_message_total);
+
+						break;
+					case '999':
+						$('.jq-login-before').show();
+
+						$('.jq-login-after').hide();
+						break;
+
+				}
+
+			}
+
+		}
+
+	});
+
+};
+
+function checkPhone(namber) {
+
+	var phone = namber
+
+	if (!(/^1[34578]\d{9}$/.test(phone))) {
+
+		alert("手机号码有误，请重填");
+
+		_phone_key = false;
+
+		return false;
+
+	} else {
+
+		_phone_key = true;
+
+	}
+
+};
+
+$('.ajax-btn-login').click(function () {
+
+	var _parent = $(this).parents('.ajax-login-form');
+
+	var _account = _parent.find('[name=account]').val();
+
+	var _passwd = _parent.find('[name=passwd]').val();
+
+	$.ajax({
+
+		url: "/api/Api/login",
+
+		type: "POST",
+
+		data: JSON.stringify({
+
+			account: _account,
+
+			passwd: _passwd
+
+		}),
+
+		success: function (data) {
+
+			if (data.status) {
+
+				switch (data.status) {
+
+					case 100:
+
+						setCookie(data.body.sess);
+
+						console.log(data.body.sess);
+
+						alert('登入成功');
+
+						userInformation()
+
+						break;
+
+					case '999':
+
+						alert('帐号或密码错误');
+
+				}
+
+			}
+
+		}
+
+	});
+
+});
+
+$('.ajax-btn-logout').click(function () {
+
+	$.ajax({
+
+		url: "/api/Api/logout",
+
+		type: "GET",
+
+		success: function (data) {
+
+			if (data.status) {
+
+				switch (data.status) {
+
+					case 100:
+
+						clearCookie();
+
+						alert('登出成功');
+
+						$('.jq-login-before').show();
+
+						$('.jq-login-after').hide();
+
+						$('.jq-member-nam').empty();
+
+						window.location.href = '/Home-Index.html';
+
+						break;
+
+				}
+
+			}
+
+		}
+
+	});
+
+});
+
+$('.ajax-btn-pcb').click(function () {
+
+	var _pcb_phone = $('.jq-pop-service').find('[name=phone-num]').val();
+
+	var _pcb_message = $('.jq-pop-service').find('[name=message]').val();
+
+	checkPhone(_pcb_phone);
+
+	if (_phone_key == true) {
+
+		$.ajax({
+
+			url: "/api/Api/addPhonecCallBack?sess=" + sessionStorage.session,
+
+			type: "POST",
+
+			data: JSON.stringify({
+
+				pcb_phone: _pcb_phone,
+
+				pcb_message: _pcb_message
+
+			}),
+
+			success: function (data) {
+
+				if (data.status) {
+
+					switch (data.status) {
+
+						case 100:
+
+							alert('问题已送出，后续将会有专人客服与您联系。');
+
+							window.location.reload();
+
+							break;
+
+					}
+
+				}
+
+			}
+
+		});
+
+	}
+
+});
+
+$('.jq-btn-pop-register').click(function () {
+
+	$.ajax({
+
+		url: "/api/Api/getCaptcha",
+
+		type: "GET",
+
+		success: function (data) {
+
+			$(".jq-pop-register").find('.jq-captcha-container').find('img').remove();
+
+			var _each = $(".jq-pop-register").find('.jq-captcha-container');
+
+			if (data.status) {
+
+				switch (data.status) {
+
+					case 100:
+
+						_each.append(
+
+							data.body.image
+
+						)
+
+						break;
+
+				}
+
+			}
+
+		}
+
+	});
+
+});
+
+$('.ajax-btn-register').click(function () {
+
+	var _username = $('.jq-pop-register').find('[name=username]').val();
+
+	var _invitecode = $('.jq-pop-register').find('[name=invitecode]').val();
+
+	var _account = $('.jq-pop-register').find('[name=account]').val();
+
+	var _password = $('.jq-pop-register').find('[name=password]').val();
+
+	var _captcha = $('.jq-pop-register').find('[name=captcha]').val();
+
+	$.ajax({
+
+		url: "/api/Api/registered/" + _invitecode,
+
+		type: "POST",
+
+		data: JSON.stringify({
+
+			name: _username,
+
+			account: _account,
+
+			passwd: _password,
+
+			captcha: _captcha
+
+		}),
+
+		success: function (data) {
+
+
+			if (data.status) {
+
+				switch (data.status) {
+
+					case 100:
+
+						alert('註冊成功');
+
+						window.location.reload();
+
+						break;
+
+					case '999':
+
+						alert(data.message);
+
+				}
+
+			}
+
+		},
+
+		error: function (data) {
+
+			console.log(data)
+
+		}
+
+	});
+
+});
+
+$(function () {
+
+	$.ajax({
+
+		url: "/api/Api/getFooterInfo",
+
+		type: "GET",
+
+		success: function (data) {
+
+			var _each_wechat = $('.jq-service-wechat');
+
+			var _each_QQ = $('.jq-service-QQ')
+
+			if (data.status) {
+
+				switch (data.status) {
+
+					case 100:
+
+						_each_wechat.append(
+
+							'<a href="javascript:void(0)" class="btn btn-bg-blue">微信客服</a>' +
+
+							'<div class="img-container">' +
+
+							'<img class="img absolute-center-xs" src="images/webconfig/' + data.body.list.wechat_qr_image.wc_value + '" alt="">' +
+
+							'</div>'
+
+						)
+
+						_each_QQ.append(
+
+							'<a href="javascript:void(0)" class="btn btn-bg-blue">QQ客服</a>' +
+
+							'<div class="img-container">' +
+
+							'<img class="img absolute-center-xs" src="images/webconfig/' + data.body.list.qq_qr_image.wc_value + '" alt="">' +
+
+							'</div>'
+
+						)
+
+				}
+
+			}
+
+		}
+
+	});
+
+
+	if (sessionStorage.session == undefined || sessionStorage.session == '') {
+
+		$('.jq-login-before').show();
+
+		$('.jq-login-after').hide();
+
+	} else {
+
+		userInformation()
+
+	}
+
+
 });
--- js/script.js
+++ js/script.js
@@ -1,210 +1,210 @@
-var Live = {
-
-  Global: {
-
-    intital: function () {
-
-      $('.jq-cancelBubble').on('click', function (event) {
-        event.cancelBubble = true;
-        event.stopPropagation();
-      });
-
-      $('.jq-nav-option').click(function () {
-        $('.jq-nav-option').removeClass('active');
-        $(this).addClass('active');
-      });
-
-      $('.jq-login-member-detail').on('click', function () {
-        if ($(this).hasClass('active')) {
-          $(this).find('.jq-member-list').removeClass('active');
-          $(this).removeClass('active');
-        } else {
-          $(this).find('.jq-member-list').addClass('active');
-          $(this).addClass('active');
-        };
-      });
-
-      $('.jq-btn-pop-register').click(function () {
-        $('.jq-pop-window').addClass('active');
-        $('.jq-pop-register').addClass('active');
-      });
-
-      $('.jq-pop-btn-service').click(function () {
-        $('.jq-pop-window').addClass('active');
-        $('.jq-pop-service').addClass('active');
-      });
-
-      $('.jq-pop-window').click(function () {
-        if ($(this).find('.jq-pop-message').hasClass('active')) {} else {
-          $('.jq-pop-window').removeClass('active');
-          $('.jq-pop-register').removeClass('active');
-          $('.jq-pop-service').removeClass('active');
-          $('.jq-pop-true-false').removeClass('active');
-          $('.jq-pop-true-false').find('.jq-title').empty();
-          $('.jq-pop-true-false').find('.jq-detail').empty();
-        }
-      });
-
-      $('.jq-pop-close').click(function () {
-        $('.jq-pop-window').removeClass('active');
-        $('.jq-pop-register').removeClass('active');
-        $('.jq-pop-service').removeClass('active');
-      });
-
-      $(window).click(function () {
-        $('.jq-login-member-detail').removeClass('active');
-        $('.jq-member-list').removeClass('active');
-      });
-
-    },
-
-    owl_banner: function () {
-      $('.owl-banner').owlCarousel({
-        loop: true,
-        margin: 0,
-        nav: false,
-        responsive: {
-          0: {
-            items: 1
-          },
-          600: {
-            items: 1
-          },
-          1000: {
-            items: 1
-          }
-        }
-      });
-    },
-
-    datepicker: function () {
-      $(function () {
-        $("#from").datepicker({
-          defaultDate: "+1w",
-          changeMonth: true,
-          numberOfMonths: 2,
-          dateFormat: "yy-mm-dd",
-          onClose: function (selectedDate) {
-            $("#to").datepicker("option", "minDate", selectedDate);
-          }
-        });
-        $("#to").datepicker({
-          defaultDate: "+1w",
-          changeMonth: true,
-          numberOfMonths: 2,
-          dateFormat: "yy-mm-dd",
-          onClose: function (selectedDate) {
-            $("#from").datepicker("option", "maxDate", selectedDate);
-          }
-        });
-      });
-    },
-
-    member: function () {
-
-      var action_length = $('.jq-member-action-list').find('li').length - 1;
-      var action_height = 55 + (73 * action_length);
-      $('.jq-member-action-list').parent().css('min-height', action_height + 'px');
-
-      $('.jq-member-tag').click(function () {
-        var key = $(this).attr('data-password');
-        console.log(key);
-        $('.jq-member-tag').removeClass('active');
-        $(this).addClass('active');
-        $('[type=password]').val('');
-        if (key == 'user') {
-          $('.jq-user-password').show();
-          $('.jq-user-cashpassword-first').hide();
-          $('.jq-user-cashpassword').hide();
-        } else if (key == 'cash') {
-          $('.jq-user-password').hide();
-        };
-      });
-
-      $('.jq-member-option').click(function () {
-        daily_key = $(this).attr('data-daily-option');
-        $('.jq-member-option').removeClass('active');
-        $(this).addClass('active');
-        if (daily_key == '1,2') {
-          $('[data-daily-table]').hide();
-          $('[data-daily-table=1]').show();
-        } else if (daily_key == '3') {
-          $('[data-daily-table]').hide();
-          $('[data-daily-table=2]').show();
-        } else if (daily_key == '6') {
-          $('[data-daily-table]').hide();
-          $('[data-daily-table=3]').show();
-        } else if (daily_key == '4,5') {
-          $('[data-daily-table]').hide();
-          $('[data-daily-table=4]').show();
-        }
-      });
-
-      $('.jq-btn-card-add').click(function () {
-        $('.jq-pop-window').addClass('active');
-        $('.jq-pop-bankcard').addClass('active');
-      });
-
-      $('.jq-input-radio').change(function () {
-        $('.jq-input-radio').parent().removeClass('checked');
-        $(this).parent().addClass('checked')
-      });
-
-      $('.jq-btn-message').click(function () {
-        $('.jq-pop-window').addClass('active');
-        $('.jq-pop-message').addClass('active');
-      });
-
-      $(document).on('click', '.jq-btn-message-delete', function () {
-        delete_key = $(this)
-        $('.jq-pop-window').addClass('active');
-        $('.jq-pop-true-false').addClass('active');
-        $('.jq-pop-true-false').find('.jq-title').html('删除讯息');
-        $('.jq-pop-true-false').find('.jq-detail').html('是否确认删除此站内讯息');
-      })
-
-      $(document).on('click', '.jq-btn-message-view', function () {
-        $('.jq-pop-window').addClass('active');
-        $('.jq-pop-message-view').addClass('active');
-        $(".jq-pop-message-view").find('.form-group').find('.row').remove();
-      });
-
-      $('.jq-pop-window').click(function () {
-        if ($(this).find('.jq-pop-message').hasClass('active')) {} else {
-          $('.jq-pop-window').removeClass('active');
-          $('.jq-pop-message-view').removeClass('active');
-          $('.jq-pop-bankcard').removeClass('active');
-          $('.jq-pop-true-false').removeClass('active');
-        }
-      });
-
-      $('.jq-pop-close').click(function () {
-        $('.jq-pop-window').removeClass('active');
-        $('.jq-pop-message').removeClass('active');
-        $('.jq-pop-message-view').removeClass('active');
-        $('.jq-pop-bankcard').removeClass('active');
-      });
-
-    },
-
-    notice_event: function () {
-
-      $(document).on('click', '.jq-btn-notice-more', function () {
-        if ($(this).hasClass('open')) {
-          $(this).parents('.jq-notice-group').find('.jq-detail').removeClass('open');
-          $(this).removeClass('open');
-          $(this).empty();
-          $(this).html('更多内容 >');
-        } else {
-          $(this).parents('.jq-notice-group').find('.jq-detail').addClass('open');
-          $(this).addClass('open');
-          $(this).empty();
-          $(this).html('收合讯息 >');
-        }
-      });
-
-    },
-
-  },
-
+var Live = {
+
+  Global: {
+
+    intital: function () {
+
+      $('.jq-cancelBubble').on('click', function (event) {
+        event.cancelBubble = true;
+        event.stopPropagation();
+      });
+
+      $('.jq-nav-option').click(function () {
+        $('.jq-nav-option').removeClass('active');
+        $(this).addClass('active');
+      });
+
+      $('.jq-login-member-detail').on('click', function () {
+        if ($(this).hasClass('active')) {
+          $(this).find('.jq-member-list').removeClass('active');
+          $(this).removeClass('active');
+        } else {
+          $(this).find('.jq-member-list').addClass('active');
+          $(this).addClass('active');
+        };
+      });
+
+      $('.jq-btn-pop-register').click(function () {
+        $('.jq-pop-window').addClass('active');
+        $('.jq-pop-register').addClass('active');
+      });
+
+      $('.jq-pop-btn-service').click(function () {
+        $('.jq-pop-window').addClass('active');
+        $('.jq-pop-service').addClass('active');
+      });
+
+      $('.jq-pop-window').click(function () {
+        if ($(this).find('.jq-pop-message').hasClass('active')) {} else {
+          $('.jq-pop-window').removeClass('active');
+          $('.jq-pop-register').removeClass('active');
+          $('.jq-pop-service').removeClass('active');
+          $('.jq-pop-true-false').removeClass('active');
+          $('.jq-pop-true-false').find('.jq-title').empty();
+          $('.jq-pop-true-false').find('.jq-detail').empty();
+        }
+      });
+
+      $('.jq-pop-close').click(function () {
+        $('.jq-pop-window').removeClass('active');
+        $('.jq-pop-register').removeClass('active');
+        $('.jq-pop-service').removeClass('active');
+      });
+
+      $(window).click(function () {
+        $('.jq-login-member-detail').removeClass('active');
+        $('.jq-member-list').removeClass('active');
+      });
+
+    },
+
+    owl_banner: function () {
+      $('.owl-banner').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 1
+          },
+          1000: {
+            items: 1
+          }
+        }
+      });
+    },
+
+    datepicker: function () {
+      $(function () {
+        $("#from").datepicker({
+          defaultDate: "+1w",
+          changeMonth: true,
+          numberOfMonths: 2,
+          dateFormat: "yy-mm-dd",
+          onClose: function (selectedDate) {
+            $("#to").datepicker("option", "minDate", selectedDate);
+          }
+        });
+        $("#to").datepicker({
+          defaultDate: "+1w",
+          changeMonth: true,
+          numberOfMonths: 2,
+          dateFormat: "yy-mm-dd",
+          onClose: function (selectedDate) {
+            $("#from").datepicker("option", "maxDate", selectedDate);
+          }
+        });
+      });
+    },
+
+    member: function () {
+
+      var action_length = $('.jq-member-action-list').find('li').length - 1;
+      var action_height = 55 + (73 * action_length);
+      $('.jq-member-action-list').parent().css('min-height', action_height + 'px');
+
+      $('.jq-member-tag').click(function () {
+        var key = $(this).attr('data-password');
+        console.log(key);
+        $('.jq-member-tag').removeClass('active');
+        $(this).addClass('active');
+        $('[type=password]').val('');
+        if (key == 'user') {
+          $('.jq-user-password').show();
+          $('.jq-user-cashpassword-first').hide();
+          $('.jq-user-cashpassword').hide();
+        } else if (key == 'cash') {
+          $('.jq-user-password').hide();
+        };
+      });
+
+      $('.jq-member-option').click(function () {
+        daily_key = $(this).attr('data-daily-option');
+        $('.jq-member-option').removeClass('active');
+        $(this).addClass('active');
+        if (daily_key == '1,2') {
+          $('[data-daily-table]').hide();
+          $('[data-daily-table=1]').show();
+        } else if (daily_key == '3') {
+          $('[data-daily-table]').hide();
+          $('[data-daily-table=2]').show();
+        } else if (daily_key == '6') {
+          $('[data-daily-table]').hide();
+          $('[data-daily-table=3]').show();
+        } else if (daily_key == '4,5') {
+          $('[data-daily-table]').hide();
+          $('[data-daily-table=4]').show();
+        }
+      });
+
+      $('.jq-btn-card-add').click(function () {
+        $('.jq-pop-window').addClass('active');
+        $('.jq-pop-bankcard').addClass('active');
+      });
+
+      $('.jq-input-radio').change(function () {
+        $('.jq-input-radio').parent().removeClass('checked');
+        $(this).parent().addClass('checked')
+      });
+
+      $('.jq-btn-message').click(function () {
+        $('.jq-pop-window').addClass('active');
+        $('.jq-pop-message').addClass('active');
+      });
+
+      $(document).on('click', '.jq-btn-message-delete', function () {
+        delete_key = $(this)
+        $('.jq-pop-window').addClass('active');
+        $('.jq-pop-true-false').addClass('active');
+        $('.jq-pop-true-false').find('.jq-title').html('删除讯息');
+        $('.jq-pop-true-false').find('.jq-detail').html('是否确认删除此站内讯息');
+      })
+
+      $(document).on('click', '.jq-btn-message-view', function () {
+        $('.jq-pop-window').addClass('active');
+        $('.jq-pop-message-view').addClass('active');
+        $(".jq-pop-message-view").find('.form-group').find('.row').remove();
+      });
+
+      $('.jq-pop-window').click(function () {
+        if ($(this).find('.jq-pop-message').hasClass('active')) {} else {
+          $('.jq-pop-window').removeClass('active');
+          $('.jq-pop-message-view').removeClass('active');
+          $('.jq-pop-bankcard').removeClass('active');
+          $('.jq-pop-true-false').removeClass('active');
+        }
+      });
+
+      $('.jq-pop-close').click(function () {
+        $('.jq-pop-window').removeClass('active');
+        $('.jq-pop-message').removeClass('active');
+        $('.jq-pop-message-view').removeClass('active');
+        $('.jq-pop-bankcard').removeClass('active');
+      });
+
+    },
+
+    notice_event: function () {
+
+      $(document).on('click', '.jq-btn-notice-more', function () {
+        if ($(this).hasClass('open')) {
+          $(this).parents('.jq-notice-group').find('.jq-detail').removeClass('open');
+          $(this).removeClass('open');
+          $(this).empty();
+          $(this).html('更多内容 >');
+        } else {
+          $(this).parents('.jq-notice-group').find('.jq-detail').addClass('open');
+          $(this).addClass('open');
+          $(this).empty();
+          $(this).html('收合讯息 >');
+        }
+      });
+
+    },
+
+  },
+
 }
