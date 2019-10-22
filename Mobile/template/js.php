a448d88051beb01aebcc0cce46439012cf9f3fcb
vavarrm <vavarrm@gmail.com>
2019-10-22 12:35:24 +0800

    西改登入

--- Mobile/template/js.php
+++ Mobile/template/js.php
@@ -1,7 +1,7 @@
 <script src="js/jquery.min.js"></script>
-<script src="js/script.js"></script>
+<script src="/Mobile/js/script.js"></script>
 <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-<script src="/js/main.js"></script>
+<script src="/Mobile/js/main.js"></script>
 <script>
 		Live.Global.intital();
 </script>
